ff24edd198ad80a2f6f781a80880f271ed1960f3
Lucie Augier <lucieaugier28@gmail.com>
2023-11-30 03:14:20 -0500

    Revert "Add About page"
    This reverts commit 39d73586577d7001def925c7e0b405140bbd9398.

--- src/components/AboutPage.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <p>Page About</p>
-</template>
--- src/components/ButtonGeneric.vue
+++ src/components/ButtonGeneric.vue
@@ -15,75 +15,39 @@ export default {
   props: {
     buttonText: {
       type: String,
-      default: "À propos"
+      default: "À propos" // Texte par défaut du bouton
     },
     buttonClass: {
       type: String,
-      default: "general"
+      default: "general" // Classe CSS par défaut du bouton
     }
   },
-
   methods: {
     handleClick() {
-      if (this.buttonText == "À propos") {
-        this.$router.push("/about");
-      }
       console.log("Bouton cliqué !");
-    },
-    handleMouseEnter(event) {
-      const relX = event.clientX - this.$refs.button.getBoundingClientRect().left;
-      const relY = event.clientY - this.$refs.button.getBoundingClientRect().top;
-      this.$refs.circle.style.left = `${relX}px`;
-      this.$refs.circle.style.top = `${relY}px`;
-    },
-    handleMouseLeave() {
-      this.$refs.circle.style.width = "0";
-      this.$refs.circle.style.height = "0";
     }
   }
 };
 </script>
 
 <style scoped>
+
 .general {
   width: 8.5rem;
   height: 2.5rem;
   border: 1px solid #131412;
-  background-color: transparent;
+  background-color: #f6f6f6;
   border-radius: 1rem;
   font-family: 'inter', sans-serif;
   font-weight: 500;
   font-size: 1rem;
-  position: relative;
-}
+  }
 
-.general:hover {
-  color: #f6f6f6;
-}
+  .button-container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
 
-.button-container {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  position: relative;
-}
-
-.circle {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  display: block;
-  width: 0;
-  height: 0;
-  border-radius: 50%;
-  background-color: #131412;
-  transition: width 0.4s ease-in-out, height 0.4s ease-in-out;
-  /* transform: translate(-50%, -50%); */
-  z-index: -1;
-}
-
-.circle:hover {
-  width: 100%;
-  height: 100%;
-}
 </style>
+
